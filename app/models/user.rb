87645cceba39d8797e6a282a9b5546b29a0cc392
miikousin <daks.0817@icloud.com>
2024-06-25 15:48:30 +0900

    アクティブストレージの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,12 @@ class User < ApplicationRecord
   belongs_to :age
   belongs_to :gender
 
+  #postモデルとのアソシエーション
+  has_many :posts
+
   validates :nickname, presence: { message: 'を入力してください' }
-  validates :height, presence: { message: 'を入力してください' }, numericality: { message: 'は数値で入力してください' }
-  validates :weight, presence: { message: 'を入力してください' }, numericality: { message: 'は数値で入力してください' }
+  validates :height, presence: { message: 'を入力してください' }, numericality: { message: 'は半角数値で入力してください' }
+  validates :weight, presence: { message: 'を入力してください' }, numericality: { message: 'は半角数値で入力してください' }
   validates :age_id, numericality: { other_than: 1 }# id:1以外のときに保存できる
   validates :gender_id, numericality: { other_than: 1 }
 
